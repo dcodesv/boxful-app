837337f914c1845e65d5a4893c35f292e8d956db
diegocodesv <diegocodesv@gmail.com>
2026-01-14 20:33:55 -0600

    Ajustes modo oscuro homescreen :lipstick:

--- app/(tabs)/delivery.tsx
+++ app/(tabs)/delivery.tsx
@@ -1,15 +1,18 @@
+import bannerDark from '@/assets/images/banner-dark.webp';
 import banner from '@/assets/images/banner.webp';
 import ActionsHome from '@/components/Home/ActionsHome';
 import SendList from '@/components/Home/SendList';
 import Header from '@/components/ui/Header';
 import Text from '@/components/ui/Text';
 import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { useThemeColor } from '@/hooks/use-theme-color';
 import { Image } from "expo-image";
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const colorScheme = useColorScheme();
   const backgroundColor = useThemeColor(
     { light: Colors.light.background, dark: Colors.dark.background },
     'background'
@@ -30,7 +33,7 @@ export default function HomeScreen() {
         </View>
         <ActionsHome />
         <View className='bg-[#eee6d1] rounded-xl overflow-hidden'>
-          <Image source={banner} contentFit="cover" contentPosition="center" style={{ height: 140, width: '100%' }} />
+          <Image source={colorScheme === "dark" ? bannerDark : banner} contentFit="cover" contentPosition="center" style={{ height: 140, width: '100%' }} />
         </View>
         <SendList />
       </ScrollView>
