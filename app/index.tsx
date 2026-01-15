6764d2a78fb7a5c55f7d670d67c99ef17691a716
diegocodesv <diegocodesv@gmail.com>
2026-01-15 01:59:35 -0600

    Ajustes a banner plash

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
+import bandSplash from '@/assets/images/bandSplash.webp';
+import LogoBoxFul from '@/assets/images/logo-boxful.svg';
+import { Colors } from '@/constants/theme';
+import { useThemeColor } from '@/hooks/use-theme-color';
+import { Image } from 'expo-image';
 import { Redirect } from 'expo-router';
 import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function Index() {
   const [isLoading, setIsLoading] = useState(true); 
   const hasSeenOnboarding = false;
   const isAuthenticated = false;
+  const backgroundColor = useThemeColor({ light: Colors.light.backgroundSecondary, dark: Colors.dark.backgroundSecondary }, 'backgroundSecondary');
 
   setTimeout(() => {
     setIsLoading(false);
@@ -13,8 +19,17 @@ export default function Index() {
 
   if (isLoading) {
     return (
-      <View className="flex-1 justify-center items-center bg-white">
-        <Text className="text-orange-500 font-bold">Cargando Boxful...</Text>
+      <View className="flex-1 justify-center items-center"
+      style={{
+        backgroundColor: backgroundColor,
+      }}
+      >
+        <LogoBoxFul width={150} height={80} />
+        <Image source={bandSplash}
+        contentFit="cover"
+        contentPosition="center"
+        style={{ width: '100%', height: 300, position: 'absolute', bottom: 0 }}
+         />
       </View>
     );
   }
