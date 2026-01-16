37bd06b9e04e890fb8a19fbee8e3d0e4d21c5c9e
diegocodesv <diegocodesv@gmail.com>
2026-01-16 02:06:22 -0600

    Integración de componentes en un solo archivo para exportar

---
+++ components/index.tsx
@@ -0,0 +1,18 @@
+// Archivo para exportar todos los componentes
+
+// Componentes principales
+export { default as AlertModal } from "./AlertModal";
+export { HapticTab } from "./Haptics";
+
+// Componentes de Home
+export { default as Actions } from "./Home/Actions";
+export { default as ActionsHome } from "./Home/ActionsHome";
+export { default as SendList } from "./Home/SendList";
+
+// Componentes de UI
+export { default as AnimatedTabScreen } from "./ui/AnimatedTabScreen";
+export { default as Button } from "./ui/Button";
+export { default as Header } from "./ui/Header";
+export { default as Input } from "./ui/Input";
+export { default as Text } from "./ui/Text";
+
